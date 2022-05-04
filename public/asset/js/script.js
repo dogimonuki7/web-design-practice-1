f81ee46398b22eaeef28016a4fdecfb4a9c67d9c
dogimonuki7 <kadowakasarete@yahoo.co.jp>
2022-05-05 00:10:02 +0900

    a

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,40 +1,70 @@
-$(function () {
-  // 動きのきっかけの起点となるアニメーションの名前を定義
-  function BgFadeAnime(){
+
+
+
+
+
+// 着火点となる要素
+const headings = document.querySelectorAll('.u-fade-in');
+
+const options = {
+  threshold: 1
+};
+
+// 実行するよ
+const observer = new IntersectionObserver(showElements);
+
+// 各 .heading に到達したら発動。複数あるから forEach 使うよ。
+headings.forEach(heading => {
+  observer.observe(heading);
+});
+
+// 要素が表示されたら実行する動作
+function showElements(entries){
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // 各 .heading に .active を加える
+      entry.target.classList.add('u-fade-in__active');
+    }
+  });
+};
+
+
+
+
+
+
+
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function BgFadeAnime(){
 
     // 背景色が伸びて出現（左から右）
-  $('.u-bgLRextend-trigger').each(function(){ //bgLRextendTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
+  $('.bgLRextendTrigger').each(function(){ //bgLRextendTriggerというクラス名が
+    var elemPos = $(this).offset().top+100;//要素より、50px上の
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){
-      $(this).addClass('u-bgLRextend');// 画面内に入ったらbgLRextendというクラス名を追記
-    }else{
-      $(this).removeClass('u-bgLRextend');// 画面外に出たらbgLRextendというクラス名を外す
+      $(this).addClass('bgLRextend');// 画面内に入ったらbgLRextendというクラス名を追記
     }
-  });	
+  }); 
 
-  // 文字列を囲う子要素
-  $('.u-bgappear-trigger').each(function(){ //bgappearTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
+   // 文字列を囲う子要素
+  $('.bgappearTrigger').each(function(){ //bgappearTriggerというクラス名が
+    var elemPos = $(this).offset().top+100;//要素より、50px上の
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){
-      $(this).addClass('u-bgappear');// 画面内に入ったらbgappearというクラス名を追記
-    }else{
-      $(this).removeClass('u-bgappear');// 画面外に出たらbgappearというクラス名を外す
+      $(this).addClass('bgappear');// 画面内に入ったらbgappearというクラス名を追記
     }
-  });		
-  }
+  });   
+}
 
-  // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
-  // 画面が読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function(){
-    BgFadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-  
+
+
+$(window).scroll(function (){
+  BgFadeAnime();
+});
+
+$(window).on('load', function(){
+  BgFadeAnime();
 });
