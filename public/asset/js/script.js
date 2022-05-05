6a8205156bd1596bad9971dd118066c943cd7a7d
dogimonuki7 <kadowakasarete@yahoo.co.jp>
2022-05-05 23:31:42 +0900

    a

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,51 +1,68 @@
-const animations = document.querySelectorAll('.u-animation');
-const observer = new IntersectionObserver(callbackRouter);
-animations.forEach(observer.observe.bind(observer));
+// フェードアップ
+const fadeUps = document.querySelectorAll('.u-fade-up-trigger');
+const fadeUp = new IntersectionObserver(fadeUpCallback);
+fadeUps.forEach(fadeUp.observe.bind(fadeUp));
 
-function callbackRouter(entries, observer) {
+function fadeUpCallback(entries){
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.forEach(item => {
-        if (item === 'u-fade-up') entry.target.classList.add('u-fade-up__active');
-        if (item === 'u-fade-left') entry.target.classList.add('u-fade-left__active');
-        if (item === 'u-fade-right') entry.target.classList.add('u-fade-right__active');
-      });
-    }
-  });
-}
-
-
-// テキスト背景アニメーション
-const bgLRextends = document.querySelectorAll('.bgLRextendTrigger');
-const bgLRextend = new IntersectionObserver(bgLRextendAnime);
-
-bgLRextends.forEach( e => {
-  bgLRextend.observe( e );
-});
-
-function bgLRextendAnime(entries){
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('bgLRextend');
+      entry.target.classList.add('u-fade-up');
     }
   });
 };
 
-const bgappears = document.querySelectorAll('.bgappearTrigger');
-const bgappear = new IntersectionObserver(bgappearAnime);
+// フェードレフト
+const fadeLefts = document.querySelectorAll('.u-fade-left-trigger');
+const fadeLeft = new IntersectionObserver(fadeLeftCallback);
+fadeLefts.forEach(fadeLeft.observe.bind(fadeLeft));
 
-bgappears.forEach( e => {
-  bgappear.observe( e );
-});
-
-function bgappearAnime(entries){
+function fadeLeftCallback(entries){
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('bgappear');
+      entry.target.classList.add('u-fade-left');
     }
   });
 };
 
+// フェードライト
+const fadeRights = document.querySelectorAll('.u-fade-right-trigger');
+const fadeRight = new IntersectionObserver(fadeRightCallback);
+fadeRights.forEach(fadeRight.observe.bind(fadeRight));
+
+function fadeRightCallback(entries){
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('u-fade-right');
+    }
+  });
+};
+
+
+// 背景スライド(中身のアニメーション。遅れて表示)
+const bgLRextends = document.querySelectorAll('.u-bg-lr-extend-trigger');
+const bgLRextend = new IntersectionObserver(bgLRextendCallback);
+bgLRextends.forEach(bgLRextend.observe.bind(bgLRextend));
+
+function bgLRextendCallback(entries){
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('u-bg-lr-extend');
+    }
+  });
+};
+
+// 背景スライド(左右にスライドするアニメーション)
+const bgappears = document.querySelectorAll('.u-bg-appear-trigger');
+const bgappear = new IntersectionObserver(bgappearCallback);
+bgappears.forEach(bgappear.observe.bind(bgappear));
+
+function bgappearCallback(entries){
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('u-bg-appear');
+    }
+  });
+};
 
 
 
